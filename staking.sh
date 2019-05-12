34d6fc05b84e38a263ab45088f7aa8debd1e4be7
KingricharDV#0439 <northerncommunity1@gmail.com>
2019-05-11 21:28:00 -0400

    Update staking.sh
    Updated to new version 2.6.0

--- staking.sh
+++ staking.sh
@@ -28,8 +28,8 @@ sudo free
 sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
 cd
 
-wget https://github.com/northern-community/Northern/raw/c35f59dd79c6173e495f08b319dd5518f4b74a61/northern-cli.tar.gz
-tar -xzf northern-cli.tar.gz
+wget https://github.com/northern-community/Northern/releases/download/untagged-8d4d75f283535b10dcc5/northern2.6.0.tar.gz
+tar -xzf northern2.6.0.tar.gz
 rm -rf northern-cli.tar.gz
 
 sudo apt-get install -y ufw
