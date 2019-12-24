e4b2d1eeee0cce59a9d9e349eb52806584dc3575
KingricharDV#0439 <northerncommunity1@gmail.com>
2019-12-24 14:17:25 -0600

    Update staking.sh

--- staking.sh
+++ staking.sh
@@ -29,8 +29,8 @@ sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
 cd
 
 wget https://github.com/northern-community/Northern/releases/download/2.6.2/northern_linux.2_6_2.tar.gz
-tar -xzf northern_linux.2_6_1.tar.gz
-rm -rf northern_linux.2_6_1.tar.gz
+tar -xzf northern_linux.2_6_2.tar.gz
+rm -rf northern_linux.2_6_2.tar.gz
 
 sudo apt-get install -y ufw
 sudo ufw allow ssh/tcp
